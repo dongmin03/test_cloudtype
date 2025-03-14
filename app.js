41893ffd09de1c962db913354f08f0851516168f
김동민 <kdm37603760@gmail.com>
2025-03-14 14:50:31 +0900

    Updata: add cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello from Cloudtype!" });
